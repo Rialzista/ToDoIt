5174f5dcb24a0c1712c6804301626a8dbdde90cf
Rialzista <kostya.mihailov@gmail.com>
2015-06-15 06:25:14 +0300

    Заменил картинку (ракеты) на большую. Добавил пример использования библиотеки визарда.

--- app/src/main/java/it_aces/todoit/activities/WizardActivity.java
+++ app/src/main/java/it_aces/todoit/activities/WizardActivity.java
@@ -1,39 +1,29 @@
 package it_aces.todoit.activities;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.Toast;
 
-import it_aces.todoit.R;
+import com.github.paolorotolo.appintro.AppIntro2;
 
-public class WizardActivity extends AppCompatActivity {
+import it_aces.todoit.fragments.FirstSlide;
+
+public class WizardActivity extends AppIntro2 {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_wizard);
+    public void init(Bundle bundle) {
+        addSlide(new FirstSlide(), getApplicationContext());
+        addSlide(new FirstSlide(), getApplicationContext());
+        addSlide(new FirstSlide(), getApplicationContext());
+        addSlide(new FirstSlide(), getApplicationContext());
+
+        setVibrate(true);
+        setVibrateIntensity(30);
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_wizard, menu);
-        return true;
+    public void onDonePressed() {
+        Toast.makeText(this, "Done", Toast.LENGTH_SHORT).show();
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
